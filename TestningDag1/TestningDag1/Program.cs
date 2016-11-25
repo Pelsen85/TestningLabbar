cb3485920e5d798acd6153fb4449f20ee75887e9
Adrian Newton-Ohnell <pelsen85@gmail.com>
2016-11-25 15:00:30 +0100

    First Lab

---
+++ TestningDag1/TestningDag1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestningDag1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line;
+            double x = 1;
+
+            Console.WriteLine("Enter a number: ");
+            line = Console.ReadLine();
+
+        
+            try
+            {
+                x = Convert.ToDouble(line);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+
+            System.Console.WriteLine(" 1 / x = {0}" , x);
+            Console.ReadKey(true);
+
+        }
+    }
+}
