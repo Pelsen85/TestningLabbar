ef69bc3ff2d21cd3ffe6bd43e8f872370d252afe
Pelsen85 <pelsen85@gmail.com>
2017-01-23 12:37:31 +0100

    Add files via upload

---
+++ NumberSystemConverter/ExceptionManager.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSystemConverter
+{
+    class ExceptionManager : RomanNumeralConverter
+    {
+        public class DmInputException : Exception
+        {
+            public DmInputException() : base()
+            {
+
+            }
+
+            public DmInputException(string message) : base(message)
+            {
+
+            }
+
+            public override string Message
+            {
+                get
+                {
+                    return "Please learn to write Roman correctly! Maybe you meant to write D";
+                }
+            }
+            
+            
+            }       
+        }
+
+    public class LmInputException : Exception
+    {
+        public LmInputException() : base()
+        {
+
+        }
+
+        public LmInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write CML?";
+            }
+        }
+
+
+    }
+
+    public class ImInputException : Exception
+    {
+        public ImInputException() : base()
+        {
+
+        }
+
+        public ImInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write CMXCIX?";
+            }
+        }
+
+
+    }
+
+    public class XmInputException : Exception
+    {
+        public XmInputException() : base()
+        {
+
+        }
+
+        public XmInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write CMXC?";
+            }
+        }
+
+
+    }
+
+    public class VmInputException : Exception
+    {
+        public VmInputException() : base()
+        {
+
+        }
+
+        public VmInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write CMXCV?";
+            }
+        }
+
+
+    }
+
+    public class LdInputException : Exception
+    {
+        public LdInputException() : base()
+        {
+
+        }
+
+        public LdInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write CDL?";
+            }
+        }
+
+
+    }
+
+    public class XdInputException : Exception
+    {
+        public XdInputException() : base()
+        {
+
+        }
+
+        public XdInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write CDXC?";
+            }
+        }
+
+
+    }
+
+    public class VdInputException : Exception
+    {
+        public VdInputException() : base()
+        {
+
+        }
+
+        public VdInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write CDXCV?";
+            }
+        }
+
+
+    }
+
+    public class IdInputException : Exception
+    {
+        public IdInputException() : base()
+        {
+
+        }
+
+        public IdInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write CDXCIX?";
+            }
+        }
+
+
+    }
+
+    public class LcInputException : Exception
+    {
+        public LcInputException() : base()
+        {
+
+        }
+
+        public LcInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write L?";
+            }
+        }
+
+
+    }
+
+    public class VcInputException : Exception
+    {
+        public VcInputException() : base()
+        {
+
+        }
+
+        public VcInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write XCV?";
+            }
+        }
+
+
+    }
+
+    public class IcInputException : Exception
+    {
+        public IcInputException() : base()
+        {
+
+        }
+
+        public IcInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write XCIX?";
+            }
+        }
+
+
+    }
+
+    public class VlInputException : Exception
+    {
+        public VlInputException() : base()
+        {
+
+        }
+
+        public VlInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write XLV?";
+            }
+        }
+
+
+    }
+
+    public class IlInputException : Exception
+    {
+        public IlInputException() : base()
+        {
+
+        }
+
+        public IlInputException(string message) : base(message)
+        {
+
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Please learn to write Roman correctly! Maybe you meant to write XLIX?";
+            }
+        }
+
+
+    }
+
+    public class IiiiInputException : Exception
+    { 
+    public IiiiInputException() : base()
+    {
+
+    }
+
+    public IiiiInputException(string message) : base(message)
+        {
+
+    }
+
+    public override string Message
+    {
+        get
+        {
+            return "Please learn to write Roman correctly! Maybe you meant to write IV?";
+        }
+    }
+
+
+}
+}
+
+    
+
---
+++ NumberSystemConverter/Program.cs
@@ -0,0 +1,163 @@
+﻿#region Usings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NumberSystemConverter.ExceptionManager;
+#endregion
+// DO NOT CHANGE
+namespace NumberSystemConverter
+{
+    class Program
+    {
+        static RomanNumeralConverter converter;
+
+        static void Main(string[] args)
+        {
+            converter = new RomanNumeralConverter();
+            string errorMessage;
+            try
+            {
+                Run();
+            }
+
+            catch (OverflowException ofEx)
+            {
+                errorMessage = ofEx.Message;
+                Console.WriteLine(errorMessage);
+            }
+
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+                Console.WriteLine(errorMessage);
+            }
+        }
+
+        static void Run()
+        {
+            #region Runs the program expecting only integral inputs and calls ConvertToRomanNumeral() upon receiving
+            while (true)
+            {
+                int result;
+                Console.WriteLine("Numeral Registry");
+                Console.WriteLine("Acceptable Values     |     Non-Acceptable Values");
+                Console.WriteLine("M - 1000              |     (D, L, I, X, V) + M");
+                Console.WriteLine("D - 500               |     (L, X, V, I) + D");
+                Console.WriteLine("C - 100               |     (L, V, I) + C");
+                Console.WriteLine("L - 50                |     (V, I) + L");
+                Console.WriteLine("X - 10                |     ");
+                Console.WriteLine("V - 5                 |     ");
+                Console.WriteLine("I - 1                 |     (I) + I + I + I + I\n");
+
+                Console.WriteLine("Please enter an integer value OR roman numeral to be converted...");
+                string userInput = Console.ReadLine();
+                string[] eiwa = { "DM", "LM", "IM", "XM", "VM", "LD", "XD", "VD", "ID", "LC", "VC", "IC", "VL", "IL", "IIII" };
+
+                try
+                {
+
+                    if (userInput.All(c => c >= '0' && c <= '9') && userInput != "")
+                    {
+                        Console.WriteLine(userInput + " = " + converter.ConvertToRomanNumeral(int.Parse(userInput)));
+                    }
+                    else if (userInput.All(c => c == 'M' || c == 'D' || c == 'C' || c == 'L' || c == 'X' || c == 'V' || c == 'I') &&
+                            !eiwa.Any(c => userInput.Contains(c)))
+                    {
+                        Console.WriteLine(userInput + " = " + converter.ConvertFromRomanNumeral(userInput.ToCharArray().Select(c => c.ToString()).ToArray()));
+                    }
+                    
+                    else if (userInput.All(c => c == 'D' || c == 'M'))
+                    {
+                        throw (new DmInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'L' || c == 'M'))
+                    {
+                        throw (new LmInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'I' || c == 'M'))
+                    {
+                        throw (new ImInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'X' || c == 'M'))
+                    {
+                        throw (new XmInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'V' || c == 'M'))
+                    {
+                        throw (new VmInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'L' || c == 'D'))
+                    {
+                        throw (new LdInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'X' || c == 'D'))
+                    {
+                        throw (new XdInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'V' || c == 'D'))
+                    {
+                        throw (new VdInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'I' || c == 'D'))
+                    {
+                        throw (new IdInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'L' || c == 'C'))
+                    {
+                        throw (new LcInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'V' || c == 'C'))
+                    {
+                        throw (new VcInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'I' || c == 'C'))
+                    {
+                        throw (new IcInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'V' || c == 'L'))
+                    {
+                        throw (new VlInputException());
+                    }
+
+                    else if (userInput.All(c => c == 'I' || c == 'L'))
+                    {
+                        throw (new IlInputException());
+                    }
+
+                    else if (userInput.All(c => (c == 'I' || c == 'I' || c == 'I' || c == 'I')))
+                    {
+                        throw (new IiiiInputException());
+                    }
+
+                    Console.Write("\nPress any key...");
+                    Console.ReadKey();
+                    Console.Clear();
+                  
+                }
+
+                catch (OverflowException ofEx)
+                {
+                    throw new Exception("Don't type so many integers you moron!", ofEx.InnerException);
+                }
+
+                               
+            }
+            #endregion
+        }
+    }
+}
+
